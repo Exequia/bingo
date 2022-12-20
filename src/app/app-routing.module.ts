e83866ba7e776a87b9f732693aa2713cf6ff54e4
Fenix <fenix@hell.com>
2022-12-20 22:34:55 +0100

    routing animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { URL_GAME, URL_HOME, URL_PLAYER, URL_SHOPPING } from './config/contants';
+import {
+  URL_GAME,
+  URL_GAME_PAGE,
+  URL_HOME,
+  URL_HOME_PAGE,
+  URL_NOT_FOUND,
+  URL_NOT_FOUND_PAGE,
+  URL_PLAYER,
+  URL_PLAYER_PAGE,
+  URL_SHOPPING,
+  URL_SHOPPING_PAGE,
+} from './config/contants';
 import { GameGuard } from './guards/game/game.guard';
 import { PlayerGuard } from './guards/player/player.guard';
 import { HomeComponent } from './views/home/home.component';
@@ -10,17 +21,36 @@ import { PageNotFoundComponent } from './views/page-not-found/page-not-found.com
 import { ShoppingComponent } from './views/shopping/shopping.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [PlayerGuard] },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [PlayerGuard],
+    data: { animation: URL_HOME_PAGE },
+  },
   { path: URL_HOME, redirectTo: '', pathMatch: 'full' },
-  { path: URL_GAME, component: NewGameComponent, canActivate: [PlayerGuard, GameGuard] },
+  {
+    path: URL_GAME,
+    component: NewGameComponent,
+    canActivate: [PlayerGuard, GameGuard],
+    data: { animation: URL_GAME_PAGE },
+  },
   {
     path: URL_SHOPPING,
     component: ShoppingComponent,
     canActivate: [PlayerGuard, GameGuard],
+    data: { animation: URL_SHOPPING_PAGE },
   },
   // { path: URL_BINGO, component: GameComponent, canActivate: [PlayerGuard] },
-  { path: URL_PLAYER, component: NewPlayerComponent },
-  { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
+  {
+    path: URL_PLAYER,
+    component: NewPlayerComponent,
+    data: { animation: URL_PLAYER_PAGE },
+  },
+  {
+    path: URL_NOT_FOUND,
+    component: PageNotFoundComponent,
+    data: { animation: URL_NOT_FOUND_PAGE },
+  }, // Wildcard route for a 404 page
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
-import { selectPlayerFeature, selectProgressConfig } from './redux';
-import { Store } from '@ngrx/store';
-import { AppState } from './redux';
 import { PlayerFacade } from './redux/facades/playerFacade';
 import { ConfigFacade } from './redux/facades/configFacade';
+import { ChildrenOutletContexts } from '@angular/router';
+import { slideInAnimation } from './animations/router.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [slideInAnimation],
 })
 export class AppComponent {
   progress$ = this.configFacade.progress$;
   player$ = this.playerFacade.player$;
 
-  constructor(private readonly configFacade: ConfigFacade, private readonly playerFacade: PlayerFacade) {}
+  constructor(
+    private readonly configFacade: ConfigFacade,
+    private readonly playerFacade: PlayerFacade,
+    private readonly contexts: ChildrenOutletContexts
+  ) {}
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
+      'animation'
+    ];
+  }
 }
